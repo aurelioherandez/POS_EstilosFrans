0e567eaa83888ab0ace69892bfe917d5039c67c1
chenaoh <cdhenao@gmail.com>
2023-11-23 23:42:00 -0600

    prueba fondo

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,4 @@
 <a href="">
-    <img src="{{ asset('images/LogoBoutique.png') }}" alt="Logo" class=" shadow-lg shadow-slate-600 rounded-full h-48">
+    <img src="https://png.pngtree.com/thumb_back/fw800/background/20201024/pngtree-digital-technology-gaming-background-with-circuit-image_436086.jpg"
+        alt="Logo" class=" shadow-lg shadow-slate-600 rounded-full h-48">
 </a>
