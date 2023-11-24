f37cefa49bf27a8585c36c41c35b0b09482c1161
chenaoh <cdhenao@gmail.com>
2023-11-23 23:49:39 -0600

    prueba fondo

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,10 +1,11 @@
 <div>
     <div></div>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 px-4 sm:pt-0 bg-opacity-60 bg-[url('/public/images/FondoBoutique.png')] bg-cover bg-center h-screen">
+    <div
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 px-4 sm:pt-0 bg-opacity-60 bg-[url('https://png.pngtree.com/thumb_back/fw800/background/20201024/pngtree-digital-technology-gaming-background-with-circuit-image_436086.jpg')] bg-cover bg-center h-screen">
         <div>
             {{ $logo }}
         </div>
-    
+
         <div class="w-full sm:max-w-md mt-6 mx-5 px-6 py-4 bg-amber-400 shadow-xl overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
