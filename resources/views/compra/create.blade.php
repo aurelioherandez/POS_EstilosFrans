1d583f71c30af9b0e67892c688035b4acfbfda68
chenaoh <cdhenao@gmail.com>
2023-11-25 11:17:33 -0600

    cambio

--- resources/views/compra/create.blade.php
+++ resources/views/compra/create.blade.php
@@ -303,14 +303,12 @@
         });
 
         //Variables
-        let cont = 0;
         let subtotal = [];
         let sumas = 0;
         let iva = 0;
         let total = 0;
-
-        //Constantes
-        const impuesto = 12;
+        let impuesto = 12; // Asegúrate de que este es el valor correcto para el impuesto
+        let cont = 0;
 
         function mostrarValores() {
             let dataProducto = document.getElementById('producto_id').value.split('-');
@@ -319,7 +317,7 @@
         }
 
         function agregarProducto() {
-            //Obtener valores de los campos
+            // Obtener valores de los campos
             let idProducto = $('#producto_id').val();
             let nameProducto = $('#producto_id option:selected').text();
             let cantidad = $('#cantidad').val();
@@ -331,57 +329,57 @@
                 descuento = 0;
             }
 
-            //Validaciones 
-            //1.Para que los campos no esten vacíos
+            // Validaciones 
+            // 1. Para que los campos no esten vacíos
             // if (idProducto != '' && cantidad != '' && precioCompra != '' && precioVenta != '') {
 
-            //2. Para que los valores ingresados sean los correctos
-            if (parseInt(cantidad) > 0 && (cantidad % 1 == 0) && parseFloat(precioCompra) > 0 && parseFloat(precioVenta) >
-                0) {
+                // 2. Para que los valores ingresados sean los correctos
+                if (parseInt(cantidad) > 0 && (cantidad % 1 == 0) && parseFloat(precioCompra) > 0 && parseFloat(
+                    precioVenta) > 0) {
 
-                //3. Para que el precio de compra sea menor que el precio de venta
-                if (parseFloat(precioVenta) > parseFloat(precioCompra)) {
-                    //Calcular valores
-                    subtotal[cont] = round(cantidad * precioCompra);
-                    sumas += subtotal[cont];
-                    iva = round(sumas / 100 * impuesto);
-                    total = round(sumas + iva);
+                    // 3. Para que el precio de compra sea menor que el precio de venta
+                    if (parseFloat(precioVenta) > parseFloat(precioCompra)) {
+                        // Calcular valores
+                        subtotal[cont] = round(cantidad * precioCompra);
+                        sumas += subtotal[cont];
+                        iva = round(sumas / 100 * impuesto);
+                        total = round(sumas + iva);
 
-                    //Crear la fila
-                    let fila = '<tr id="fila' + cont + '">' +
-                        '<th>' + (cont + 1) + '</th>' +
-                        '<td><input type="hidden" name="arrayidproducto[]" value="' + idProducto + '">' + nameProducto +
-                        '</td>' +
-                        '<td><input type="hidden" name="arraycantidad[]" value="' + cantidad + '">' + cantidad +
-                        '</td>' +
-                        '<td><input type="hidden" name="arraypreciocompra[]" value="' + precioCompra + '">' +
-                        precioCompra + '</td>' +
-                        '<td><input type="hidden" name="arrayprecioventa[]" value="' + precioVenta + '">' +
-                        precioVenta + '</td>' +
-                        '<td>' + subtotal[cont] + '</td>' +
-                        '<td><button class="btn btn-danger" type="button" onClick="eliminarProducto(' + cont +
-                        ')"><i class="fas fa-trash"></i></button></td>' +
-                        '</tr>';
+                        // Crear la fila
+                        let fila = '<tr id="fila' + cont + '">' +
+                            '<th>' + (cont + 1) + '</th>' +
+                            '<td><input type="hidden" name="arrayidproducto[]" value="' + idProducto + '">' + nameProducto +
+                            '</td>' +
+                            '<td><input type="hidden" name="arraycantidad[]" value="' + cantidad + '">' + cantidad +
+                            '</td>' +
+                            '<td><input type="hidden" name="arraypreciocompra[]" value="' + precioCompra + '">' +
+                            precioCompra + '</td>' +
+                            '<td><input type="hidden" name="arrayprecioventa[]" value="' + precioVenta + '">' +
+                            precioVenta + '</td>' +
+                            '<td>' + subtotal[cont] + '</td>' +
+                            '<td><button class="btn btn-danger" type="button" onClick="eliminarProducto(' + cont +
+                            ')"><i class="fas fa-trash"></i></button></td>' +
+                            '</tr>';
 
-                    //Acciones después de añadir la fila
-                    $('#tabla_detalle').append(fila);
-                    limpiarCampos();
-                    cont++;
-                    disableButtons();
+                        // Acciones después de añadir la fila
+                        $('#tabla_detalle').append(fila);
+                        limpiarCampos();
+                        cont++;
+                        disableButtons();
 
-                    //Mostrar los campos calculados
-                    $('#sumas').html(sumas);
-                    $('#iva').html(iva);
-                    $('#total').html(total);
-                    $('#impuesto').val(iva);
-                    $('#inputTotal').val(total);
+                        // Mostrar los campos calculados
+                        $('#sumas').html(sumas);
+                        $('#iva').html(iva);
+                        $('#total').html(total);
+                        $('#impuesto').val(iva);
+                        $('#inputTotal').val(total);
+                    } else {
+                        showModal('Precio de compra incorrecto');
+                    }
+
                 } else {
-                    showModal('Precio de compra incorrecto');
+                    showModal('Valores incorrectos');
                 }
-
-            } else {
-                showModal('Valores incorrectos');
-            }
 
             // } else {
             //     showModal('Le faltan campos por llenar');
